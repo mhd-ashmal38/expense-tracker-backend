b1b1677cb3ff831024e3237582da12edddd210ff
mhd-ashmal38 <mhdashmal38@gmail.com>
2025-04-30 10:49:41 +0530

    Create register API

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const coonectDB = require("./config/db");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -19,6 +19,8 @@ app.use(
 app.use(express.json());
 
 connectDB(); // Connect to MongoDB
+
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
