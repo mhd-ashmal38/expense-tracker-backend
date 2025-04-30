5cd5cd227be1e4fb9481b10c997ca184ff435561
mhd-ashmal38 <mhdashmal38@gmail.com>
2025-04-30 10:50:21 +0530

    Create login API

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,7 +41,27 @@ exports.registerUser = async (req, res) => {
 };
 
 // login user
-exports.loginUser = async (req, res) => {};
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    try{
+        const user = await User.findOne({email});
+        if(!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    }catch(err) {
+        return res.status(500).json({message: "Error logging in user", error: err.message });
+    }
+};
 
 // get user info
 exports.getUserInfo = async (req, res) => {};
