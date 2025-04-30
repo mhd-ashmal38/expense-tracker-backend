c271e066d669610d7701bd52157b769554cf2712
mhd-ashmal38 <mhdashmal38@gmail.com>
2025-04-30 12:03:18 +0530

    Create get user info API

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,18 @@
-const express = require('express');
+const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
 
 const {
-    registerUser,
-    loginUser,
-    getUserInfo,
+  registerUser,
+  loginUser,
+  getUserInfo,
 } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
 router.post("/login", loginUser);
-// router.get("/getUser", protect, getUserInfo);
+
+router.get("/getUser", protect, getUserInfo);
 
 module.exports = router;
